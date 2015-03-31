c9b0dd0915f5aa1ddf0a2c61389b44ce1c07c8f7
Robert Rees <shudderfix@gmail.com>
2015-03-31 22:45:21 +0100

    Build and eploys via gulp

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,9 @@ var gulp = require('gulp');
 var gulpPlugins = require('auto-plug')('gulp');
 var jade = require('jade');
 
-gulp.task('default', function() {
+gulp.task('default', ['deploy']);
+
+gulp.task('build', function() {
     return gulp
         .src('templates/**/*.jade')
         .pipe(gulpPlugins.jade({
@@ -10,4 +12,8 @@ gulp.task('default', function() {
             pretty: true
             }))
         .pipe(gulp.dest('build/'))
+});
+
+gulp.task('deploy', ['build'], function() {
+    return gulpPlugins.run('surge build acidic-cent.surge.sh').exec();
 });
