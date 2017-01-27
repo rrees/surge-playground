6adc21cdea7d0873c3aff39512c98190838629d4
Robert Rees <shudderfix@gmail.com>
2017-01-27 07:19:31 +0000

    Sets up a working payload dump from a static page to a cloud function

--- src/index.js
+++ src/index.js
@@ -1,2 +1,9 @@
 
 console.log('Hello world');
+
+fetch('https://us-central1-rrees-gcf-playground.cloudfunctions.net/jsoncors')
+  .then(function(response) {
+    response.json().then(function(json) {
+        document.querySelector('#output').innerHTML = JSON.stringify(json, null, 4);
+    });
+  });
